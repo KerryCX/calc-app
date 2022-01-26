30bed9c54658b434c300fc09f321cb56a149cdd4
Kerry Clements <kerryclements@Kerrys-Air.local.tld>
2022-01-26 19:36:15 +0000

    number outputting to display but need to put into an array

--- calc-app/src/components/Button.jsx
+++ calc-app/src/components/Button.jsx
@@ -2,13 +2,12 @@ import './Button.css';
 import {useState} from 'react';
 
 export const Button = ({value, classy, childToParent}) => {
-    const [newValue, setValue] = useState(value)
 
+    const [newValue, setValue] = useState(value)
     return(
         <button className = {classy} onClick = { () => {
             setValue(value)
             childToParent(newValue)
-            console.log(newValue)
         }}>{value}
         </button>
     );
--- calc-app/src/components/ButtonsGrid.jsx
+++ calc-app/src/components/ButtonsGrid.jsx
@@ -4,30 +4,25 @@ import {useState} from 'react';
 export const ButtonsGrid = () => {
 
    const calculatorKeys = ['1','2','3','4','5','6','7','8','9','-','0','+','X','=','/']
-   const [keyedValue, setKeyValue] = useState('');
-   const [newDisplay, setDisplay] = useState([])
+   const [newDisplay, setDisplay] = useState([]);
 
 
    const childToParent = (sentKeyValue) => {
-        setKeyValue(sentKeyValue);
-        console.log(sentKeyValue)
-        const displayArray = [...newDisplay, ...keyedValue];
-        setDisplay(displayArray);
-        console.log(newDisplay)
+        setDisplay(sentKeyValue)
    }
 
-
-console.log(newDisplay)
    return(
        <section className='grid-styling'>
             {calculatorKeys.map((currentKey, index) => (
                 <Button
                     key={index}
                     value={currentKey}
+                    classy='button-symbol'
                     childToParent={childToParent}
                 />
                 ))}
             <div className="display-position display-answer">{newDisplay}</div>
        </section>
    );
+
 }
